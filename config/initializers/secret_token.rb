25ef451fb1481035d5dac1a6a601327a358d2347
Eric Draut <edraut@gmail.com>
2010-09-14 00:00:50 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sketchwhiteboard::Application.config.secret_token = '2abba59a1cb267d8124b4e0690c8b7434f2dc7636064ce55d06e050b8fc3b6af8bed66845f3b6cb8793779e18f23db13de0fa9f39027bf724393ccb76a200f4e'
